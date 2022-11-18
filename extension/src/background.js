cb5156bd9b36a0f6dcf9cda765105b596e85f340
SCube19 <kk418331@students.mimuw.edu.pl>
2022-11-19 00:09:57 +0100

    messages

--- extension/src/background.js
+++ extension/src/background.js
@@ -5,17 +5,7 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
-});
+chrome.webRequest.onBeforeRequest.addListener((req) => {
+    console.log(req.tabId);
+    chrome.tabs.sendMessage(req.tabId, { data: req });
+}, { urls: ["<all_urls>"] })
