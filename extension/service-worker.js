9f3a08dc1ba1c4d48dcf570c7ed4c69116179650
SCube19 <kk418331@students.mimuw.edu.pl>
2022-11-18 21:07:00 +0100

    for witkor

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -10,3 +10,15 @@ importScripts('service-worker-utils.js')
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
 // The path should be relative to the file `manifest.json`.
+
+chrome.webRequest.onBeforeRequest.addListener((data) => {
+    console.log("siusiak");
+    console.log(data);
+    console.log(document.getElementById("content").innerHTML);
+}, { urls: ["<all_urls>"] })
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    console.log("siusiak2");
+    console.log(tab);
+    console.log(tabId);
+})
