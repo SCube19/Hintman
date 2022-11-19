61026472204ba4b8228dc0f76deaae6f6d3ec748
SCube19 <kk418331@students.mimuw.edu.pl>
2022-11-19 13:12:57 +0100

    deleted mess

--- extension/src/contentScript.js
+++
@@ -1,7 +0,0 @@
-(() => {
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { data } = obj;
-        console.log(data);
-        document.getElementById('qrcode').innerHTML = "xd";
-    });
-})();
