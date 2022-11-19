26c73f8ace2f0f139ec7a253ff5c50d408f05c90
Wiktor Zdrojewski <wiktorzd@wp.pl>
2022-11-19 06:58:16 +0100

    feat(extension): add correct version to my-extension folder

--- my-extension/src/popup.js
+++ my-extension/src/popup.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import './popup.css';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword  } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 import { QRCodeSVG } from '@cheprasov/qrcode';
 
 (function () {
@@ -11,90 +13,16 @@ import { QRCodeSVG } from '@cheprasov/qrcode';
   // To get storage access, we have to mention it in `permissions` property of manifest.json file
   // More information on Permissions can we found at
   // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: (cb) => {
-      chrome.storage.sync.get(['count'], (result) => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
 
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
+  function setup() {
+    document.getElementById('logout').addEventListener('click', logout)
+    document.getElementById('goToRegister').addEventListener('click', goToRegister)
+    document.getElementById('goToLogin').addEventListener('click', goToLogin)
 
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
-
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get((count) => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            (response) => {
-              console.log('Current count value passed to contentScript file');
-              console.log(response);
-            }
-          );
-        });
-      });
-    });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get((count) => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
+    if (localStorage.getItem("email") !== null) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("loggedIn").style.display = "block";
+    }
   }
 
   function updateQR(url) {
@@ -114,7 +42,43 @@ import { QRCodeSVG } from '@cheprasov/qrcode';
     divElement.innerHTML = qrSVG.toString();
   }
 
-  document.addEventListener('DOMContentLoaded', restoreCounter);
+  document.addEventListener('DOMContentLoaded', setup);
+  document.addEventListener('DOMContentLoaded', formFunction);
+  document.addEventListener('DOMContentLoaded', formFunctionRegister);
+
+  function formFunction() {
+    document.getElementById("login_form").addEventListener('submit', e => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        // console.log([...data.entries()]);
+        tryLogin([...data.entries()])
+      });
+  }
+
+  function formFunctionRegister() {
+    document.getElementById("register_form").addEventListener('submit', e => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        // console.log([...data.entries()]);
+        tryRegister([...data.entries()])
+      });
+  }
+
+  function logout() {
+    localStorage.clear()
+    document.getElementById("loggedIn").style.display = "none";
+    document.getElementById("login").style.display = "block";
+  }
+
+  function goToRegister() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("register").style.display = "block";
+  }
+
+  function goToLogin() {
+    document.getElementById("register").style.display = "none";
+    document.getElementById("login").style.display = "block";
+  }
 
   let website_url = undefined;
 
@@ -154,3 +118,85 @@ import { QRCodeSVG } from '@cheprasov/qrcode';
   //   }
   // );
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------- FIREBASE ------------------------------------
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxO5C3fG4AyhcZn_wKrYgSGJxer0RatF4",
+  authDomain: "hintman-f86e1.firebaseapp.com",
+  projectId: "hintman-f86e1",
+  storageBucket: "hintman-f86e1.appspot.com",
+  messagingSenderId: "366527975531",
+  appId: "1:366527975531:web:96ec38736b8e185215e12e",
+  measurementId: "G-S1JKPR54K8"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+function tryLogin(data) {
+  signInWithEmailAndPassword(auth, data[0][1], data[1][1])
+    .then((userCredential) => {
+      const user = userCredential.user;
+      localStorage.setItem('email', user.email);
+      document.getElementById("login").style.display = "none";
+      document.getElementById("loggedIn").style.display = "block";
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Authentication failed");
+    });
+}
+
+function tryRegister(data) {
+  if (data[1][1] !== data[2][1]) {
+      alert("Passwords aren't equal");
+      return;
+  }
+  createUserWithEmailAndPassword (auth, data[0][1], data[1][1])
+  .then((userCredential) => {
+      const user = userCredential.user;
+      localStorage.setItem('email', user.email);
+      document.getElementById("register").style.display = "none";
+      document.getElementById("loggedIn").style.display = "block";
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Invalid email address");
+    });
+}
